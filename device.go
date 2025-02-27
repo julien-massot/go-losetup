d455c5794d7dafbfa4ca8db39be563f250d8f365
Julien Massot <julien.massot@collabora.com>
2025-02-27 16:03:07 +0100

    Add function to set an alternate block size
    Newer storage such as UFS or NVMe use a 4096 bytes block size
    storage.
    
    A disk image made for these devices will not work with the default
    block size (512 bytes). For example, the GPT partition will not be read/
    or written correctly.

--- device.go
+++ device.go
@@ -3,6 +3,7 @@ package losetup
 import (
 	"fmt"
 	"os"
+	"golang.org/x/sys/unix"
 )
 
 // Device represents a loop device /dev/loop#
@@ -29,4 +30,22 @@ func (device Device) open() (*os.File, error) {
 // Path returns the path to the loopback device
 func (device Device) Path() string {
 	return fmt.Sprintf(DeviceFormatString, device.number)
+}
+
+// Change the default block size (512 bytes) for a loop device,
+// returns an error if the blocksize can't be set.
+func (device Device) SetBlockSize(blockSize uint32) error {
+	f, err := device.open()
+	if err != nil {
+		return fmt.Errorf("couldn't open loop device: %v", err)
+	}
+
+	defer f.Close()
+
+	_, _, err = unix.Syscall(unix.SYS_IOCTL, f.Fd(), SetBlockSize, uintptr(blockSize))
+	if err != 0 {
+		return fmt.Errorf("Failed to set block size: %w", err)
+	}
+
+	return nil
 }
